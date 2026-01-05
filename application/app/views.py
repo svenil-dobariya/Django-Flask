b599c0298aaab430039b2f5a2c377389556335ab
svenil-dobariya <dobariyasvenil@gmail.com>
2026-01-05 10:32:15 +0000

    add css

--- application/app/views.py
+++ application/app/views.py
@@ -8,4 +8,4 @@ def newapp(request):
     return HttpResponse("<h1 style='color: red;'>This is a new app view!</h1>")
 
 def about(request):
-    return HttpResponse("<h1>name: nilesh</h1><h2>age: 17</h2> <h3>Hobby: cricket</h3> <p> parul university </p>")
+    return HttpResponse("<h1 style='color: blue; size: 56px;'>name: nilesh</h1><h2 style='color: green; size: 50px;'>age: Minor</h2> <h3 style='color: purple; size: 48px;'>Hobby: If you know you know....zzzzz</h3> <p style='color: orange; size: 36px;'> parul university(Sadly)</p>")
