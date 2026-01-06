13be9d9a7d8f4b088732ee915b222bd549be82e4
svenil-dobariya <dobariyasvenil@gmail.com>
2026-01-06 08:14:16 +0000

    cv section added

--- application/app/urls.py
+++ application/app/urls.py
@@ -2,9 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('newapp/', views.newapp, name='newapp'),
-    path('about/', views.about, name='about'),
-    path('home/', views.home_page, name='home_page'),
-    path('contact/', views.contact, name='contact'),
+    path('', views.cview, name='cview'),
 ]
