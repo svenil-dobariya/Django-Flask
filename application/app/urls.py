c01b978826547b463dcf143af1a9462256059b87
svenil-dobariya <dobariyasvenil@gmail.com>
2026-01-06 05:36:11 +0000

    add css and html of contact and index

--- application/app/urls.py
+++ application/app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('newapp/', views.newapp, name='newapp'),
     path('about/', views.about, name='about'),
+    path('home/', views.home_page, name='home_page'),
+    path('contact/', views.contact, name='contact'),
 ]
--- application/app/views.py
+++ application/app/views.py
@@ -2,10 +2,39 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Welcome to the Home Page!</h1>")
+    data = [
+        {'name': 'Inception', 'genre': 'Sci-Fi'},
+        {'name': 'The Dark Knight', 'genre': 'Action'},
+        {'name': 'Interstellar', 'genre': 'Sci-Fi'},
+        {'name': 'Pulp Fiction', 'genre': 'Crime'},
+        {'name': 'The Shawshank Redemption', 'genre': 'Drama'},
+        {'name': 'The Godfather', 'genre': 'Crime'},
+        {'name': 'Forrest Gump', 'genre': 'Drama'},
+        {'name': 'The Matrix', 'genre': 'Sci-Fi'},
+    ]
+    return render(request, 'index.html', {'movies': data})
 
 def newapp(request):
     return HttpResponse("<h1 style='color: red;'>This is a new app view!</h1>")
 
 def about(request):
     return HttpResponse("<h1 style='color: blue; size: 56px;'>name: nilesh</h1><h2 style='color: green; size: 50px;'>age: Minor</h2> <h3 style='color: purple; size: 48px;'>Hobby: If you know you know....zzzzz</h3> <p style='color: orange; size: 36px;'> parul university(Sadly)</p>")
+
+def home_page(request):
+    data = [
+        {'name': 'Inception', 'genre': 'Sci-Fi'},
+        {'name': 'The Dark Knight', 'genre': 'Action'},
+        {'name': 'Interstellar', 'genre': 'Sci-Fi'},
+        {'name': 'Pulp Fiction', 'genre': 'Crime'},
+        {'name': 'The Shawshank Redemption', 'genre': 'Drama'},
+    ]
+    return render(request, 'index.html' , {'movies': data})
+
+def contact(request):
+    contact = {
+        'name' : 'nilesh',
+        'age' : 'minor',
+        'hobby' : 'If you know you know....zzzzz',
+        'university' : 'parul university(Sadly)'
+    }
+    return render(request, 'contact.html', {'contact': contact})
